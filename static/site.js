9b543c97918afba51d9443492c645b31134f1519
Oleg Lavrovsky <oleg@utou.ch>
2018-11-03 10:35:46 +0100

    Dynamic loading (WIP)

--- static/site.js
+++ static/site.js
@@ -1,20 +1,39 @@
-$.getJSON('/motive/json', function(data) {
-  $out = $('#motive');
-  console.log(data);
+jQuery(function($){
+
+$('div[data-tag]').each(function() {
+  tag = $(this).attr('data-tag');
+  $.getJSON('/' + tag + '/json', function(data) {
+    render_form(tag, data);
+  });
+});
+
+function render_form(tag, data, inputtype) {
+  if (typeof(inputtype) === 'undefined')
+    inputtype = 'checkbox';
+  $out = $('#' + tag);
+  // console.log(data);
   col_ix = 0; per_col = Math.round(data.length / 4);
+
   get_col = function() {
-    return $out.append('<div class="col-sm-3" />').find('div:last');
+    return $out.append('<div class="col-sm-3" />')
+               .find('div:last');
   };
   $col = get_col();
   $.each(data, function() {
     $col.append(
-      '<div class="item">' +
+      '<div class="form-check">' +
+        '<label class="form-check-label">' +
+        '<input class="form-check-input" ' +
+        'name="' + this.Code + '" ' +
+        'value="1" type="checkbox">' +
         this.Title +
-      '</div>'
+      '</label></div>'
     );
     if (++col_ix == per_col) {
       $col = get_col();
       col_ix = 0;
     }
   });
+}
+
 });
